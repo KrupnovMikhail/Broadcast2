4e57e25898792fad497ba753800e948955b18965
Mikhail Krupnov <krupnovmn@gmail.com>
2023-11-26 02:06:07 +0300

    add progress bar

--- app/src/main/java/com/krupnov/broadcast2/MainActivity.kt
+++ app/src/main/java/com/krupnov/broadcast2/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.krupnov.broadcast2
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
+import android.widget.ProgressBar
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var progressBar: ProgressBar
+
+    private val receiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            if (intent?.action == "loaded") {
+                val percent = intent.getIntExtra("percent", 0)
+                progressBar.progress = percent
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        progressBar = findViewById(R.id.progressBar)
+
+        val intentFilter = IntentFilter().apply {
+            addAction("loaded")
+        }
+        registerReceiver(receiver, intentFilter)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(receiver)
     }
 }
